057c4811aebc39af11c8183cab0a0688eabe1942
Hleb Prakhniski <thane.crios@gmail.com>
2021-10-12 17:02:26 +0300

    appended ContactCampaignItem

--- app/Models/ContactCampaignItem.php
+++ app/Models/ContactCampaignItem.php
@@ -10,4 +10,26 @@ class ContactCampaignItem extends Model
     use HasFactory;
 
     protected $fillable = ['campaign_item_id', 'contact_id', 'external_service_id', 'external_service_status'];
+
+    protected $appends = ['contact_plus_status', 'campaign_item_plus_status'];
+
+    public function campignItem()
+    {
+        return $this->hasOne(CampaignItem::class, 'id', 'campaign_item_id');
+    }
+
+    public function contact()
+    {
+        return $this->hasOne(Contact::class, 'id', 'contact_id');
+    }
+
+    public function getContactPlusStatusAttribute()
+    {
+        return $this->contact->email . '[' . ($this->external_service_status ?? 'none') . ']';
+    }
+
+    public function getCampaignItemPlusStatusAttribute()
+    {
+        return $this->campignItem->name . '[' . ($this->external_service_status ?? 'none') . ']';
+    }
 }
