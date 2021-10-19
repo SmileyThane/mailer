c0d9f42a032401d7f962e68a1d9c498678e6e667
Hleb Prakhnitski <thane.crios@gmail.com>
2021-10-19 05:38:45 +0300

    updated sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,7 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('contact') }}'><i class='nav-icon la la-question'></i> Contacts</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign') }}'><i class='nav-icon la la-question'></i> Campaigns</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign-item') }}'><i class='nav-icon la la-question'></i> Campaign items</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('template') }}'><i class='nav-icon la la-question'></i> Templates</a></li>
+{{--<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>--}}
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('template') }}'><i class='nav-icon la la-file-text-o'></i> Email templates</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('contact') }}'><i class='nav-icon la la-user'></i> Contacts</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('contact-group') }}'><i class='nav-icon la la-users-cog'></i> Contact groups</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign') }}'><i class='nav-icon la la-business-time'></i> Campaigns</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('campaign-item') }}'><i class='nav-icon la la-address-book-o'></i> Campaign Sequence</a></li>
