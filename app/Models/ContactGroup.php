c56fd88e4747b36245dda110cc042b31e6ddd47c
Hleb Prakhnitski <thane.crios@gmail.com>
2021-10-19 05:36:10 +0300

    created contact group model

---
+++ app/Models/ContactGroup.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactGroup extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+
+    protected $fillable = ['name', 'user_id'];
+
+    protected $hidden = ['user_id'];
+}
