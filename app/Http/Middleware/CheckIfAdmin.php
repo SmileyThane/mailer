9c1dca8d37b836c97aed741fe7379a238febab08
Hleb Prakhnitski <thane.crios@gmail.com>
2021-11-30 22:23:09 +0300

    updated middleware

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class CheckIfAdmin
 {
@@ -49,8 +50,8 @@ class CheckIfAdmin
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
