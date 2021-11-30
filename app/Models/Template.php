2e4a030f95000237f8fd2f2c259760ba2cd13d8d
Hleb Prakhnitski <thane.crios@gmail.com>
2021-11-30 22:25:01 +0300

    minor fix

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+    use CrudTrait;
     use HasFactory;
 
     protected $fillable = ['name', 'data', 'user_id', 'subject'];
